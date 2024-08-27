af53783fffa0c14ae68460f5d7b75bd5c3048206
Satyaprakash <satyaasingh001@gmail.com>
2024-08-28 02:32:34 +0530

    add preloader

--- js/javascript.js
+++ js/javascript.js
@@ -1,17 +1,23 @@
-$('.owl-carousel').owlCarousel({
-    loop: true,
-    margin: 10,
-    nav: true,
-    responsive: {
-        0: {
-            items: 1
-        },
-        600: {
-            items: 3
-        },
-        1000: {
-            items: 4
-        }
-    }
-})
+$(".owl-carousel").owlCarousel({
+  loop: true,
+  margin: 10,
+  nav: true,
+  responsive: {
+    0: {
+      items: 1,
+    },
+    600: {
+      items: 3,
+    },
+    1000: {
+      items: 4,
+    },
+  },
+});
 
+let loader = document.getElementById("preloader");
+function myLoader() {
+  loader.style.display = "none";
+}
+
+myLoader();
